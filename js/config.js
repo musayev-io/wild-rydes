fc2aa960724a4f4d3b840153df0492ee389dfa21
Musa <musa@musayev.io>
2019-10-07 10:24:12 -0400

    Configured Cognito settings

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_2jRx8Oo1R', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7pn4i77u2tfei2jhs227iab8qo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
